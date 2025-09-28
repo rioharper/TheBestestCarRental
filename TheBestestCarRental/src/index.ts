5b135ff8af35fa3a837f1f0544ca4653f797ee41
AlexHartman2005 <aohartma@syr.edu>
2025-09-28 17:08:30 -0400

    Adding classes, just tpyes no methods yet

---
+++ TheBestestCarRental/src/index.ts
@@ -0,0 +1,77 @@
+import express from "express"
+
+
+class Car {
+    year: number;
+    size: string;
+    color: string;
+    make : string;
+    model : string;
+    availability : boolean;
+
+    constructor(year: number, size: string, color: string, model: string, make: string, availability: boolean) {
+        this.year = year;
+        this.size = size;
+        this.color = color;
+        this.make = make;
+        this.model = model;
+        this.availability = availability;
+    }
+}
+
+class User {
+    userId: string;
+    password: string;
+
+    constructor(userID: string, password: string) {
+        this.userId = userID;
+        this.password = password;
+    }
+}
+
+class customer extends User {
+        checkedOut: boolean;
+        driversLicenseNumber: number;
+
+    constructor(userId: string, password: string, driversLicenseNumber: number, checkedOut: boolean) {
+        super(userId, password);
+        this.driversLicenseNumber = driversLicenseNumber;
+        this.checkedOut = checkedOut;
+    }
+}
+
+class Admin extends User {
+    constructor(userId: string, password: string) {
+        super(userId, password)
+    }
+}
+
+
+class Reservation {
+    reservationId : string
+    startDate : Date;
+    endDate : Date;
+    status : boolean;
+
+    constructor(reservationId : string, startDate : Date, endDate : Date, status : boolean) {
+        this.reservationId = reservationId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+    }
+}
+
+
+class Payment {
+    method: string;
+    payId: string;
+    amount: number;
+    status: boolean;
+
+    constructor(method: string, payId: string, amount: number, status: boolean) {
+        this.method = method;
+        this.payId = payId;
+        this.amount = amount;
+        this.status = status;
+    }
+}
